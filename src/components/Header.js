f97cb187680a49da9c9a65e5ba114b25ddb30a7c
Boatera <81436213+Boatera@users.noreply.github.com>
2022-06-19 22:48:34 +0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div}>
             <h1 class="title">Call a Friend</h1>
-            <p classN="subtitle">Your Friendly Contact App</p>
+            <p class="subtitle">Your Friendly Contact App</p>
             <hr></hr>
         </div>
     );
